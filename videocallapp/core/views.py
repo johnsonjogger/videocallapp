a80de5cf816139da08edae8be5a0d2ec356bbb89
lucky <luckyetefia@gmail.com>
2021-11-23 19:29:24 -0800

    comment render function in views

--- videocallapp/core/views.py
+++ videocallapp/core/views.py
@@ -1,11 +1,10 @@
-from django.conf import settings
 from django.shortcuts import render
-
+from django.http import HttpResponse
 def home(request):
-  
-    return render(request, 'core/download_app.html', {})
+    return HttpResponse("<h1> Hello Django </h1>")
 
 
 
 def download_app(request):
-    return render(request, 'core/index.html', {})
+     return HttpResponse("<h1> Download Page </h1>")
+    #return render(request, 'core/download_app.html', {})
