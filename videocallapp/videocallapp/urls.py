c6fc1a5d012af6082a397a6f2835a18180ae8e61
lucky <luckyetefia@gmail.com>
2021-12-29 20:14:31 -0800

    changed default admin url

--- videocallapp/videocallapp/urls.py
+++ videocallapp/videocallapp/urls.py
@@ -1,12 +1,14 @@
 
+from . import views 
 from django.contrib import admin
 from django.conf import settings
+from django.views.generic import TemplateView
 from django.conf.urls.static import static
-from django.urls import path, include
-from django.urls import path
+from django.urls import path, include, re_path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    re_path(r'^robots\.txt$', TemplateView.as_view(template_name="videocallapp/robots.txt", content_type='text/plain')),
+    path('ms-teams/secret/admin/', admin.site.urls),
     path('', include('core.urls', namespace='core')),
 ]
 
@@ -14,3 +16,8 @@ urlpatterns = [
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+
+handler404 = views.error_404_view
+handler500 = views.error_500_view
